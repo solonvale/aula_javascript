1e8865db629d40f4c8be3b44b3d8c987d7352776
solonvale <solonvale@gmail.com>
2021-11-16 09:45:15 -0300

    aula 1

---
+++ js/main.js
@@ -0,0 +1,25 @@
+var frutas = [{ nome: "maçã", cor: "vermelha" }, { nome: "uva", cor: "roxa" }]
+console.log(frutas);
+alert(frutas[1].nome);
+
+
+//var lista = ["maçã", "pera", "laranja"];
+//lista.push("uva");
+//lista.pop();
+
+//console.log(lista);
+//console.log(lista.toString());
+//console.log(lista.join(" | "))
+
+
+
+//var nome = "Solon Vale";
+//var n1 = 5;
+//var n2 = 3;
+//var frase = "Japão é o melhor time do mundo";
+//alert(nome + " tem " + idade + " anos ");
+//alert(idade + idade2);
+//console.log(nome);
+//console.log(n1 * n2);
+//console.log(frase.toLowerCase());
+//alert(frase.replace("Japão", "Brasil"));
